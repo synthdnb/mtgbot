77571f67fd9c6ca172f8af3e055fb1ad42056872
Minkyu Kim <synthdnb@gmail.com>
2016-04-11 14:08:40 +0900

    양면카드 처리 추가

--- migrate.rb
+++ migrate.rb
@@ -3,22 +3,38 @@ require 'pry'
 data = Oj.load_file 'AllSets-x.json'
 new_data = {}
 
-data.each do |code, set|
-  cards = set["cards"]
-  cards.each do |c|
-    if c["foreignNames"] && (kname = c["foreignNames"].find{|x| x["language"] == "Korean"}) && kname["multiverseid"]
-      new_data[kname["name"].downcase.delete(" ,’'")] = {
-        "name" => kname["name"],
-        "card_id" => kname["multiverseid"],
-        "price_id" => c["multiverseid"]
-      }
-    end
-    new_data[c["name"].downcase.delete(" ,’'")] = {
-      "name" => c["name"],
-      "card_id" => c["multiverseid"],
-      "price_id" => c["multiverseid"]
-    }
+class String
+  def to_key
+    downcase.delete(" ,’'")
   end
 end
 
+data.each do |code, set|
+  cards = set["cards"]
+  cards.each do |c|
+    key = c["name"].to_key
+    new_data[key] = {
+      "lang" => "en",
+      "name" => c["name"],
+      "card_id" => c["multiverseid"],
+      "price_id" => c["multiverseid"],
+      "key_en" => key,
+    }
+    if c["layout"] == "double-faced"
+      new_data[key]["back"] = c["names"].reject{|x| x == c["name"]}.first.to_key
+    end
+    if c["foreignNames"] && (kname = c["foreignNames"].find{|x| x["language"] == "Korean"}) && kname["multiverseid"]
+      k_key = kname["name"].to_key
+      new_data[k_key] = {
+        "lang" => "ko",
+        "name" => kname["name"],
+        "card_id" => kname["multiverseid"],
+        "price_id" => c["multiverseid"],
+        "key_en" => key,
+        "key_ko" => k_key,
+      }
+      new_data[key]["key_ko"] = k_key
+    end
+  end
+end
 File.write "all.json", Oj.dump(new_data)
--- mtgbot.rb
+++ mtgbot.rb
@@ -11,19 +11,29 @@ end
 
 data = Oj.load_file 'all.json'
 client = Slack::RealTime::Client.new
-url = "http://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid="
+
+def card_link card
+  "<http://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=#{card["card_id"]}|[#{card["name"]}]>"
+end
+
 client.on :message do |msg|
   if msg.channel == "C0U7FHW66" && msg.text =~ /\[\[(.+)\]\]/
     key = $1.downcase.delete(" ,’'")
     cards = data.select{|x| x.include? key}
 
-    if data[key]
-      client.web_client.chat_postMessage username: "매직봇", channel: msg.channel, text: "<#{url}#{data[key]["card_id"]}|[#{data[key]["name"]}]>"
+    if data[key] || cards.count == 1
+      exact = data[key] || cards.values.first
+      if data[exact["key_en"]]["back"]
+        back = data[data[data[exact["key_en"]]["back"]]["key_#{exact["lang"]}"]]
+        client.web_client.chat_postMessage username: "매직봇", channel: msg.channel, text: "#{card_link(exact)} <=> #{card_link(back)}"
+      else
+        client.web_client.chat_postMessage username: "매직봇", channel: msg.channel, text: card_link(exact) 
+      end
     else
       if cards.count > 20
         client.web_client.chat_postMessage username: "매직봇", channel: msg.channel, text: "#{cards.count}장의 검색결과가 있습니다."
       elsif cards.count > 0
-        client.web_client.chat_postMessage username: "매직봇", channel: msg.channel, text: cards.map{|k, v| "<#{url}#{v["card_id"]}|[#{v["name"]}]>"}.join(" "), unfurl_media: cards.count == 1
+        client.web_client.chat_postMessage username: "매직봇", channel: msg.channel, text: cards.map{|k, v| card_link(v)}.join(" "), unfurl_media: false
       end
     end
   end
